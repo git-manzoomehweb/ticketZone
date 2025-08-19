e1344ade68cbd5dc2e277fd338676555da212d7c
fatmhJamali <kmg.d103@gmail.com>
2025-08-19 09:27:45 +0330

    currency fixed.

--- js/ticketZone-core.js
+++ js/ticketZone-core.js
@@ -243,19 +243,19 @@ const headerMenuClose = document.querySelector(".header-menu-close");
 const bars3 = document.querySelector(".bars3");
 
 if (window.innerWidth >= 1024) {
-  headerMenuClose.addEventListener("click", function () {
+  headerMenuClose?.addEventListener("click", function () {
     headerMenu.style.visibility = "hidden";
     headerMenu.style.opacity = "0";
   });
-  bars3.addEventListener("click", function () {
+  bars3?.addEventListener("click", function () {
     headerMenu.style.visibility = "visible";
     headerMenu.style.opacity = "1";
   });
 } else {
-  headerMenuClose.addEventListener("click", function () {
+  headerMenuClose?.addEventListener("click", function () {
     headerMenu.style.transform = "translateX(1024px)";
   });
-  bars3.addEventListener("click", function () {
+  bars3?.addEventListener("click", function () {
     headerMenu.style.transform = "translateX(0)";
   });
 }
@@ -264,11 +264,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleDropdowns = document.querySelectorAll(".toggle-dropdown");
   const dropdownIcons = document.querySelectorAll(".dropdown-icon");
 
-  toggleDropdowns.forEach((toggle, index) => {
+  toggleDropdowns?.forEach((toggle, index) => {
     const submenu = toggle.nextElementSibling;
     const dropdownIcon = dropdownIcons[index];
 
-    toggle.addEventListener("click", function () {
+    toggle?.addEventListener("click", function () {
       dropdownIcon.classList.toggle("rotate-180");
 
       if (submenu.style.maxHeight) {
